bc4d1e2e3e02db583f816081fadd033157b565ef
Alexander Jansen <a.jansen@reanmo.com>
2024-09-19 20:14:59 +0200

    Value based Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,9 +6,13 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 
+class MockCoffeesService {}
+
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
-    providers: [CoffeesService],
+    providers: [
+        { provide: CoffeesService, useValue: new MockCoffeesService() },
+    ],
     controllers: [CoffeesController],
     exports: [CoffeesService],
 })
