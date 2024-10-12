c38bd2cdd7e5e6ff7175ff54fd76bad247ad10e3
Alexander Jansen <a.jansen@reanmo.com>
2024-10-12 16:22:14 +0200

    Introducing the Swagger Module

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -19,6 +20,18 @@ async function bootstrap() {
         new WrapResponseInterceptor(),
         // new TimeoutInterceptor(),
     );
+
+    // Setting up Swagger document
+    const options = new DocumentBuilder()
+        .setTitle('Iluvcoffee')
+        .setDescription('Coffee application')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
