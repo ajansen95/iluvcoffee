d88f16385d6108b63ff0627992c3b43746c46804
Alexander Jansen <a.jansen@reanmo.com>
2024-10-23 22:12:18 +0200

    Adapt .prettierrc to eslint

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,18 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesService } from './coffees.service';
+import { DataSource } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Flavor } from './entities/flavor.entity';
+import { Coffee } from './entities/coffee.entity';
+import { COFFEE_BRANDS } from './coffees.constant';
 
 describe('CoffeesService', () => {
-    let service: CoffeesService;
+  let service: CoffeesService;
 
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [CoffeesService],
-        }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CoffeesService,
+        { provide: DataSource, useValue: {} },
+        { provide: getRepositoryToken(Flavor), useValue: {} }, // 👈
+        { provide: getRepositoryToken(Coffee), useValue: {} }, // 👈
+        { provide: COFFEE_BRANDS, useValue: {} }, // 👈
+      ],
+    }).compile();
 
-        service = module.get<CoffeesService>(CoffeesService);
-    });
+    service = module.get<CoffeesService>(CoffeesService);
+  });
 
-    it('should be defined', () => {
-        expect(service).toBeDefined();
-    });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
 });
